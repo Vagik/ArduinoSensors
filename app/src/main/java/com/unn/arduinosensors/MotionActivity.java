36a1e160787af54872855dffbbe6af9be1da1d79
Vagik <simonyan.vagik@yandex.ru>
2018-04-12 15:38:53 +0300

    Add ProgressBar and graphView

--- app/src/main/java/com/unn/arduinosensors/MotionActivity.java
+++ app/src/main/java/com/unn/arduinosensors/MotionActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MotionActivity extends AppCompatActivity {
 
     @Override
@@ -14,5 +18,13 @@ public class MotionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Motion Sensor");
         setSupportActionBar(toolbar);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        DataPoint[] dataPoints = new DataPoint[30];
+        for(int i = 0; i < 30; i++){
+            dataPoints[i] = new DataPoint(i, Math.random()*5);
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+        graph.addSeries(series);
     }
 }
--- app/src/main/java/com/unn/arduinosensors/TemperatureActivity.java
+++ app/src/main/java/com/unn/arduinosensors/TemperatureActivity.java
@@ -1,10 +1,20 @@
 package com.unn.arduinosensors;
 
+import android.print.PrintAttributes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class TemperatureActivity extends AppCompatActivity {
+    private int tempCoef = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,26 @@ public class TemperatureActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Temperature");
         setSupportActionBar(toolbar);
+
+        int temperature = rand(0, 50);
+        TextView tempView = ((TextView) findViewById(R.id.tempTextView));
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        int size = progressBar.getMeasuredWidth();
+
+        tempView.setText(Integer.toString(temperature));
+        progressBar.setProgress(tempCoef * temperature);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        DataPoint[] dataPoints = new DataPoint[30];
+        for (int i = 0; i < 30; i++) {
+            dataPoints[i] = new DataPoint(i, Math.random() * 5);
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+        graph.addSeries(series);
+    }
+
+    private int rand(int min, int max) {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
     }
 }
